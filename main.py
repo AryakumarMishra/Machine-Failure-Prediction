6155973df5d5f798c50005653e9643a7e761de1d
Aryakumar Mishra <aryakumar.ajaymishra@gmail.com>
2025-07-02 16:49:11 +0530

    Minor Fix

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+
 from data_loader import load_data, basic_eda
 from feature_engineering import encode_and_engineer
 from model_training import train_model
